fbaa4baba9dd8d5be93923a4fa5a1e505e300cd5
Tripham <Tripmph05786@fpt.edu.vn>
2019-05-12 21:17:40 +0700

    lan nay up phai len

--- app/src/main/java/com/phamminhtri/luyentap/test.java
+++ app/src/main/java/com/phamminhtri/luyentap/test.java
@@ -13,5 +13,6 @@ public class test extends AppCompatActivity {
         Log.d("TAG", "onCreate: ");
         Log.d("TAG", "onCreate: ");
         Log.d("TAG", "onCreate: ");
+        Log.d("TAG", "onCreate: ");
     }
 }
