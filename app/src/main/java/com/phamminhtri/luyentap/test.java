d9fbd0fa1f76ce75f7025c76f0f3bfc3ebfe34b7
Tripham <Tripmph05786@fpt.edu.vn>
2019-05-12 21:07:08 +0700

    new code

--- app/src/main/java/com/phamminhtri/luyentap/test.java
+++ app/src/main/java/com/phamminhtri/luyentap/test.java
@@ -2,6 +2,7 @@ package com.phamminhtri.luyentap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class test extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Log.d("TAG", "onCreate: ");
     }
 }
